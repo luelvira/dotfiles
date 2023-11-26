69f9d3f0fc3e842f78ec305677f1511ac8c4df4b
Lucas Elvira Martin <lucaselvira96@gmail.com>
2023-11-26 20:35:16 +0100

    Add the configuration for i3wm

--- config/polybar/uptime.sh
+++ config/polybar/uptime.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-
-uptime -p
+uptime | awk -F, '{sub(".*up ",x,$1);print $1}'
--- config/rofi/scripts/launch.sh
+++ config/rofi/scripts/launch.sh
@@ -5,12 +5,11 @@ menu="$1"
 if [ "$menu" = "filebrowser" ]; then
     rofi -modi "Filebrowser:~/.config/rofi/scripts/filebrowser.sh $2" -show Filebrowser 
 elif [ "$menu" = "appmenu" ]; then
-    rofi -show drun -theme default
+    rofi -show drun
 elif [ "$menu" = "powermenu" ]; then
     rofi -modi 'Powermenu:~/.config/rofi/scripts/powermenu.sh' -show Powermenu -theme powermenu -location 3 -xoffset -28 -yoffset 50 -icon-theme "kora"
 elif [ "$menu" = "tabmenu" ]; then
-   # ~/.config/rofi/scripts/tabmenu.sh
-   rofi -show window -theme tabmenu
+   rofi -show window
 elif [ "$menu" = "run" ]; then
 	rofi -show run 
 elif [ "$menu" = "search" ]; then
