299bab6f0d1e66d8e871c280dd0303cde1f8d52c
Lucas Elvira Martín <lucaselvira96@gmail.com>
2023-12-28 21:08:18 +0100

    2023-12-28T21:08

--- emacs/early-init.el
+++
@@ -1,5 +0,0 @@
-(setq package-enable-at-startup nil)
-
-;; Change the user-emacs-directory to keep unwanted things out of ~/.emacs.d
-(setq user-emacs-directory (expand-file-name "~/.cache/emacs/")
-      url-history-file (expand-file-name "url/history" user-emacs-directory))
